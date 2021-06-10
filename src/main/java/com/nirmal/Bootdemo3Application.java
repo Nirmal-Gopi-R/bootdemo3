8aab1295690bd0ccc5306e7cca584c1958d7c488
Nirmal Gopi <nirmalgopi585@gmail.com>
2021-06-10 09:53:41 +0530

    first commit boot demo 3

---
+++ src/main/java/com/nirmal/Bootdemo3Application.java
@@ -0,0 +1,13 @@
+package com.nirmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootdemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootdemo3Application.class, args);
+	}
+
+}
